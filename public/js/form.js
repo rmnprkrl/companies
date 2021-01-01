d09b7c9b4f1011f1132f414d2df46c0f2cadd300
hugurp <hugurp@users.noreply.github.com>
2021-01-01 19:04:24 +0000

    deploy: 4d8b4b912c1a21e3c7c7ea47ab09fa4acd7c790d

--- public/js/form.js
+++
@@ -1,5 +0,0 @@
-import FormEmailPassword from './components/form/form-email-password.js'
-import FormUpdateEmail from './components/form/form-update-email.js'
-import FormJob from './components/form/form-job.js'
-
-export default {}
