539ebae72519f561d14752a2a5978b1fd1d4b43c
hugurp <hugurp@users.noreply.github.com>
2021-01-05 09:50:17 +0000

    deploy: 538c25cfbb37fdb5901a62da2bb5ac0e6430641d

--- public/js/notification.js
+++
@@ -1,5 +0,0 @@
-import Notification from './components/notification/index.js'
-import NotificationSystem from './components/notification/system.js'
-import NotificationItem from './components/notification/item.js'
-
-export default {}
--- public/js/providers.js
+++
@@ -1,6 +0,0 @@
-import Recruitee from './components/providers/recruitee.js'
-import Greenhouse from './components/providers/greenhouse.js'
-import Personio from './components/providers/personio.js'
-import SmartRecruiters from './components/providers/smartrecruiters.js'
-
-export default {}
